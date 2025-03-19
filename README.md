72195e20667214d53012710229e6499e8a1faf03
Meptl <git-client@meptl.com>
2025-03-19 11:13:04 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Allchat
+Firefox plugin to talk to all the chats.
+
+https://github.com/user-attachments/assets/e7f49e0c-5bc6-4ffc-889d-81307d91df22
---
+++ content.js
@@ -0,0 +1,50 @@
+console.log("Loaded")
+
+console.log(browser.runtime.getManifest());
+
+const domainInfo = {
+    'chatgpt.com': {
+        textArea: '.ProseMirror > p',
+        sendButton: 'button[aria-label="Send prompt"]',
+        setMessage: (element, message) => { element.innerHTML = message; }
+    },
+    'claude.ai': {
+        textArea: '.ProseMirror > p',
+        sendButton: 'button[aria-label="Send Message"]',
+        setMessage: (element, message) => { element.innerHTML = message; }
+    },
+    'www.perplexity.ai': {
+        textArea: 'textarea.overflow-auto',
+        sendButton: 'button[aria-label="Submit"]',
+        setMessage: (element, message) => { element.value = message; }
+    },
+    'chat.deepseek.com': {
+        textArea: 'textarea#chat-input',
+        sendButton: 'div._7436101',
+        setMessage: (element, message) => { element.value = message; }
+    }
+};
+
+// window.postMessage({type:'input',message:'sup'},'*')
+// window.postMessage({type:'submit'},'*')
+
+window.addEventListener('message', function(event) {
+    if (event.data.type === 'input') {
+        const hostname = window.location.hostname;
+        const domain = Object.keys(domainInfo).find(domain => hostname.includes(domain));
+        if (domain) {
+            const chatTextAreaElem = document.querySelector(domainInfo[domain].textArea);
+            domainInfo[domain].setMessage(chatTextAreaElem, event.data.message);
+            // Inform any listeners that the text field was updated (allows
+            // clicking submit).
+            chatTextAreaElem.dispatchEvent(new Event('input', { bubbles: true }));
+        }
+    }
+    if (event.data.type === 'submit') {
+        const hostname = window.location.hostname;
+        const domain = Object.keys(domainInfo).find(domain => hostname.includes(domain));
+        const chatSendButtonElem = document.querySelector(domainInfo[domain].sendButton);
+        chatSendButtonElem.click();
+    }
+});
+
